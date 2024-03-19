c535e426112d0cc0107c7b7a29f76915d509baca
Ethan <ethanhough29@gmail.com>
2024-03-19 12:08:15 -0400

    built CRUD routes

--- server/app.py
+++ server/app.py
@@ -29,12 +29,12 @@ def index():
 # User class view
 class Users(Resource):
 
-    # Get all users - test
+    # Get all users - works
     def get(self):
         users = [user.to_dict() for user in User.query.all()]
         return make_response(users, 200)
 
-    # Create a new user - test
+    # Create a new user - works
     def post(self):
         req_data = request.get_json()
         try:
@@ -49,9 +49,37 @@ class Users(Resource):
 api.add_resource(Users, "/users")
 
 
-# User by ID view
+# User by ID view - works
 class UsersById(Resource):
-    pass
+    def get(self, id): # tested - works
+        user = User.query.filter(User.id == id).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response({"error": "User not found"}, 404)
+
+    def delete(self, id): # tested - works
+        user = User.query.filter(User.id == id).first()
+        if not user:
+            return make_response({"error": "User not found"}, 404)
+        db.session.delete(user)
+        db.session.commit()
+        return make_response({"message": "User deleted successfully"}, 200)
+
+    def patch(self, id): # tested - works
+        user = User.query.filter(User.id == id).first()
+        req_data = request.get_json()
+        if not user:
+            return make_response({"error": "User not found"}, 404)
+        try:
+            for key, value in req_data.items():
+                setattr(user, key, value)
+        except:
+            return make_response({"error": "Invalid data"}, 400)
+        db.session.commit()
+        return make_response(user.to_dict(), 200)
+    
+api.add_resource(UsersById, "/users/<int:id>")
 
 
 # Loan_Application class view
@@ -80,7 +108,35 @@ api.add_resource(LoanApplications, "/loan_applications")
 
 # Loan_Application by ID view
 class LoanApplicationsById(Resource):
-    pass
+    def get(self, id): # tested - works
+        loan_app = Loan_Application.query.filter(Loan_Application.id == id).first()
+        if loan_app:
+            return make_response(loan_app.to_dict(), 200)
+        else:
+            return make_response({"error": "Loan Application not found"}, 404)
+        
+    def delete(self, id): # tested - works
+        loan_app = Loan_Application.query.filter(Loan_Application.id == id).first()
+        if not loan_app:
+            return make_response({"error": "Loan Application not found"}, 404)
+        db.session.delete(loan_app)
+        db.session.commit()
+        return make_response({"message": "Loan Application deleted successfully"}, 200)
+    
+    def patch(self, id): # tested - works
+        loan_app = Loan_Application.query.filter(Loan_Application.id == id).first()
+        req_data = request.get_json()
+        if not loan_app:
+            return make_response({"error": "Loan Application not found"}, 404)
+        try:
+            for key, value in req_data.items():
+                setattr(loan_app, key, value)
+        except:
+            return make_response({"error": "Invalid data"}, 400)
+        db.session.commit()
+        return make_response(loan_app.to_dict(), 200)
+    
+api.add_resource(LoanApplicationsById, "/loan_applications/<int:id>")
 
 
 # Task class view
@@ -107,7 +163,35 @@ api.add_resource(Tasks, "/tasks")
 
 # Task by ID view
 class TasksById(Resource):
-    pass
+    def get(self, id): # tested - works
+        task = Task.query.filter(Task.id == id).first()
+        if task:
+            return make_response(task.to_dict(), 200)
+        else:
+            return make_response({"error": "Task not found"}, 404)
+        
+    def delete(self, id): # tested - works
+        task = Task.query.filter(Task.id == id).first()
+        if not task:
+            return make_response({"error": "Task not found"}, 404)
+        db.session.delete(task)
+        db.session.commit()
+        return make_response({"message": "Task deleted successfully"}, 200)
+    
+    def patch(self, id): # tested - works
+        task = Task.query.filter(Task.id == id).first()
+        req_data = request.get_json()
+        if not task:
+            return make_response({"error": "Task not found"}, 404)
+        try:
+            for key, value in req_data.items():
+                setattr(task, key, value)
+        except:
+            return make_response({"error": "Invalid data"}, 400)
+        db.session.commit()
+        return make_response(task.to_dict(), 200)
+    
+api.add_resource(TasksById, "/tasks/<int:id>")
 
 
 # Assigned_Task view
@@ -136,7 +220,35 @@ api.add_resource(AssignedTasks, "/assigned_tasks")
 
 # Assigned_Task by ID view
 class AssignedTasksById(Resource):
-    pass
+    def get(self, id): # tested - works
+        assigned_task = Assigned_Task.query.filter(Assigned_Task.id == id).first()
+        if assigned_task:
+            return make_response(assigned_task.to_dict(), 200)
+        else:
+            return make_response({"error": "Assigned Task not found"}, 404)
+        
+    def delete(self, id): # tested - works
+        assigned_task = Assigned_Task.query.filter(Assigned_Task.id == id).first()
+        if not assigned_task:
+            return make_response({"error": "Assigned Task not found"}, 404)
+        db.session.delete(assigned_task)
+        db.session.commit()
+        return make_response({"message": "Assigned Task deleted successfully"}, 200)
+    
+    def patch(self, id): # tested - works
+        assigned_task = Assigned_Task.query.filter(Assigned_Task.id == id).first()
+        req_data = request.get_json()
+        if not assigned_task:
+            return make_response({"error": "Assigned Task not found"}, 404)
+        try:
+            for key, value in req_data.items():
+                setattr(assigned_task, key, value)
+        except:
+            return make_response({"error": "Invalid data"}, 400)
+        db.session.commit()
+        return make_response(assigned_task.to_dict(), 200)
+    
+api.add_resource(AssignedTasksById, "/assigned_tasks/<int:id>")
 
 
 # Comment class view
@@ -159,6 +271,38 @@ class Comments(Resource):
 
 
 api.add_resource(Comments, "/comments")
+
+# Comments by ID view
+class CommentsById(Resource):
+    def get(self, id): # tested - works
+        comment = Comment.query.filter(Comment.id == id).first()
+        if comment:
+            return make_response(comment.to_dict(), 200)
+        else:
+            return make_response({"error": "Comment not found"}, 404)
+        
+    def delete(self, id): # tested - works
+        comment = Comment.query.filter(Comment.id == id).first()
+        if not comment:
+            return make_response({"error": "Comment not found"}, 404)
+        db.session.delete(comment)
+        db.session.commit()
+        return make_response({"message": "Comment deleted successfully"}, 200)
+    
+    def patch(self, id): # tested - works
+        comment = Comment.query.filter(Comment.id == id).first()
+        req_data = request.get_json()
+        if not comment:
+            return make_response({"error": "Comment not found"}, 404)
+        try:
+            for key, value in req_data.items():
+                setattr(comment, key, value)
+        except:
+            return make_response({"error": "Invalid data"}, 400)
+        db.session.commit()
+        return make_response(comment.to_dict(), 200)
+    
+api.add_resource(CommentsById, "/comments/<int:id>")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
